09a33cdbf68056f1ff274b481868f7d609dba464
M. Pennella <michael.pennella@gmail.com>
2018-09-11 20:52:09 -0700

    Added basic game progression logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,48 @@
 var game = {
-    questions = [],
+    questionData: [],
 
     // Method setting up the game splash screen and loading questions
     start() {
         // Set up splash screen in #game
+        let target = $("#game");
+        target.empty();
+
+        let startButton = $("<button>").text("Start");
+        
+        startButton.on("click", game.askNextQuestion);
+
+        target.append(startButton);
+        
+        // TODO: Determine random order of questions loaded from questionData array
+
+    },
+
+    askNextQuestion() {
+        let target = $("#game");
+        target.empty();
 
         // Placeholder
-        $("#game").text("START");
+        let questionText = "What is the answer to this question?";
 
-        // TODO: Determine random order of questions loaded from questions array
+        // Placeholder
+        let answer1Text = "Answer A";
 
+        let answer1Btn = $("<button>").text(answer1Text);
+        answer1Btn.on("click", game.showAnswer );
+
+        target.append($("<div>").text(questionText));
+        target.append(answer1Btn);
+    }, 
+
+    showAnswer() {
+        let target = $("#game");
+        target.empty();
+
+        let answerImg = $("<img>").attr( {src:"assets/images/TEST.png", alt:"Placeholder" } );
+        let answerExpText = "Detailed explanation of answer";
+
+        target.append(answerImg);
+        target.append( $("<div>").text(answerExpText) );
     }
 }
 
